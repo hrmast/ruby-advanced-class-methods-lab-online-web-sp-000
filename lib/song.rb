536d5a900fd21695b2206ccb81d98bc7c5e6da86
Heath Mast <hrmast@gmail.com>
2020-06-04 04:09:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,9 @@ class Song
   def self.new_from_filename(name, artist_name)
     #initializes a song and artist_name based on the filename format (FAILED - 1)
     song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    
 
 
   end
